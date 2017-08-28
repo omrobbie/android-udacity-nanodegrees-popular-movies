a0f1e5c0a88fd130319e5f94875768d20821fe80
omrobbie <putu.roby@yahoo.com>
2017-08-28 15:06:19 +0800

    tambahkan animasi saat masuk ke detail activity

--- app/src/main/java/com/omrobbie/popmovies/adapter/MovieViewHolder.java
+++ app/src/main/java/com/omrobbie/popmovies/adapter/MovieViewHolder.java
@@ -1,17 +1,19 @@
 package com.omrobbie.popmovies.adapter;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.google.gson.Gson;
 import com.omrobbie.popmovies.BuildConfig;
-import com.squareup.picasso.Picasso;
-
-import com.omrobbie.popmovies.R;
 import com.omrobbie.popmovies.DetailActivity;
+import com.omrobbie.popmovies.R;
 import com.omrobbie.popmovies.model.MovieItem;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,7 +39,13 @@ public class MovieViewHolder extends RecyclerView.ViewHolder {
             public void onClick(View v) {
                 Intent intent = new Intent(itemView.getContext(), DetailActivity.class);
                 intent.putExtra("movie", new Gson().toJson(data));
-                itemView.getContext().startActivity(intent);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat
+                            .makeSceneTransitionAnimation((Activity) itemView.getContext(), imgMovie, "image");
+                    itemView.getContext().startActivity(intent, activityOptionsCompat.toBundle());
+                }
+                else itemView.getContext().startActivity(intent);
             }
         });
     }
